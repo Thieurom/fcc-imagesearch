802632c5d404d16204bc6ba184752c7f42934a42
Doan Le Thieu <thieurom@gmail.com>
2016-12-03 08:14:16 +0700

    Setup deps, config app

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('It works!');
+});
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT} ...`);
+});
